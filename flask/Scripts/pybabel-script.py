c6f7122395b82bb17f2a5dcc156547c182529d87
candyash <ashenafi.legehar@gmail.com>
2014-09-22 15:55:13 +0300

    heroku deployment

--- flask/Scripts/pybabel-script.py
+++
@@ -1,9 +0,0 @@
-#!C:\Users\ashnet\Desktop\fastMonkey\flask\Scripts\python.exe
-# EASY-INSTALL-ENTRY-SCRIPT: 'Babel==1.0','console_scripts','pybabel'
-__requires__ = 'Babel==1.0'
-import sys
-from pkg_resources import load_entry_point
-
-sys.exit(
-   load_entry_point('Babel==1.0', 'console_scripts', 'pybabel')()
-)
