3f0654c349d4212f2e4bebe24874c44c96e77859
candyash <ashenafi.legehar@gmail.com>
2014-09-22 22:24:11 +0300

    heroku deployment

--- manage.py
+++ manage.py
@@ -42,8 +42,8 @@ def adduser(email, username, admin=False):
 
 
 if __name__ == '__main__':
-    port=int(os.environ.get('PORT',5000))
-    manager.run(host='0.0.0.0',port=port)
+  
+    manager.run()
 
 
 
