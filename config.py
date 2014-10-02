e711f0cb9e1cc0db47e8247205b7afaa547bdeeb
candyash <ashenafi.legehar@gmail.com>
2014-10-02 20:49:19 +0300

    flask-whooshalchemy added

--- config.py
+++ config.py
@@ -12,17 +12,20 @@ class DevelopmentConfig(Config):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 't0p s3cr3t'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+    SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 class TestingConfig(Config):
     TESTING = True
     SECRET_KEY = '3daTdaT12T'
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+    SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 class HerokuConfig(ProductionConfig):
     def init_app(cls,app):
         ProductionConfig.init_app(app)
