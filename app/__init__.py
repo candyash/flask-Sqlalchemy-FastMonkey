bbe329f9b3609a0fc5dbcfcbe4e53e797d2e9b60
candyash <ashenafi.legehar@gmail.com>
2014-10-02 19:46:21 +0300

    config file modified

--- app/__init__.py
+++ app/__init__.py
@@ -21,15 +21,24 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     
-
-    if not app.config['DEBUG'] and not app.config['TESTING'] and os.environ.get('HEROKU') is None:
-     
-        # send standard logs to syslog
+    
+    
+    if not app.debug and os.environ.get('HEROKU') is None:
         import logging
-        from logging.handlers import SysLogHandler
-        syslog_handler = SysLogHandler()
-        syslog_handler.setLevel(logging.WARNING)
-        app.logger.addHandler(syslog_handler)
+        from logging.handlers import RotatingFileHandler
+        file_handler = RotatingFileHandler('tmp/fastmonkey.log', 'a',1 * 1024 * 1024, 10)
+        file_handler.setLevel(logging.INFO)
+        file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        app.logger.addHandler(file_handler)
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('microblog startup')
+    if os.environ.get('HEROKU') is not None:
+        import logging
+        stream_handler = logging.StreamHandler()
+        app.logger.addHandler(stream_handler)
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('fastmonkey')
+
 
     bootstrap.init_app(app)
     db.init_app(app)
